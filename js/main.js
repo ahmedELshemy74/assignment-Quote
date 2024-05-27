85bb271e6eea8cbc18c4cad0882ac49cadc682c0
ahmedELshemy74 <ahmedelshemy82298@gmail.com>
2024-05-28 00:21:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+const quotes = [
+    "The best way to get started is to quit talking and begin doing. - Walt Disney",
+    "The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty. - Winston Churchill",
+    "Don’t let yesterday take up too much of today. - Will Rogers",
+    "You learn more from failure than from success. Don’t let it stop you. Failure builds character. - Unknown",
+    "It’s not whether you get knocked down, it’s whether you get up. - Vince Lombardi",
+    "If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you. - Steve Jobs",
+    "People who are crazy enough to think they can change the world, are the ones who do. - Rob Siltanen",
+    "Failure will never overtake me if my determination to succeed is strong enough. - Og Mandino",
+    "We may encounter many defeats but we must not be defeated. - Maya Angelou",
+    "Knowing is not enough; we must apply. Wishing is not enough; we must do. - Johann Wolfgang Von Goethe"
+];
+
+let lastIndex = -1;
+
+function generateRandomQuote() {
+    let index;
+    do {
+        index = Math.floor(Math.random() * quotes.length);
+    } while (index === lastIndex);
+    
+    lastIndex = index;
+    document.getElementById("quote").innerText = quotes[index];
+}
